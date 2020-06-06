92fa637dc889195ff012e1403cb4fdc75caae494
elmarek <mareks@gmx.com>
2020-06-06 15:28:40 -0700

    Last commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,22 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+
 module.exports.createSession = (req, res, next) => {
+
+  if (Object.keys(req.cookies).length === 0) {
+    return models.Sessions.create()
+    .then(data => {
+      let session = {};
+      session['hash'] = data
+      req.session = session
+      let cookies = {};
+
+      cookies['shortlyid'].value = data
+      res.cookies = cookies
+      next();
+    })
+  }
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,25 @@
 const parseCookies = (req, res, next) => {
+
+  let cookie = req.headers.cookie
+  if (!cookie) {
+    return;
+ }
+  var cookieArr = cookie.split(' ')
+
+  var cookies = {};
+  for (var i = 0; i < cookieArr.length; i++) {
+
+    var index = cookieArr[i].indexOf('=')
+    var cookieProp = cookieArr[i].slice(0, index);
+    var cookieVal = cookieArr[i].slice(index + 1);
+
+    if(cookieVal[cookieVal.length - 1] === ';') {
+      cookieVal = cookieVal.slice(0, cookieVal.length -1);
+    }
+    cookies[cookieProp] = cookieVal;
+  }
+  req.cookies = cookies
+  next();
 };
 
 module.exports = parseCookies;
